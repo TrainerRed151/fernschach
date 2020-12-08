6b0878f2537a9eb2d284c3e41ad216b155c0ff2b
Brian Pomerantz <br.pomerantz@gmail.com>
2020-12-08 09:18:52 -0500

    initial commit

---
+++ spiel/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Player(models.Model):
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=256//8)
+    salt = models.CharField(max_length=8)
+    elo = models.IntegerField(default=0)
+
+
+class Game(models.Model):
+    white = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='white')
+    black = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='black')
+    board = models.CharField(max_length=256)

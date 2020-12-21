f6176f90a6c381cc9dfe067ba8f34925ba4bb6be
Brian Pomerantz <br.pomerantz@gmail.com>
2020-12-21 00:48:50 -0500

    chessboard js with submit button

--- spiel/urls.py
+++ spiel/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from spiel.views import home
+from spiel.views import home, game
 
 urlpatterns = [
     path('', home, name='home'),
+    path('spiel/<int:game_id>/', game, name='game'),
 ]
--- spiel/views.py
+++ spiel/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from spiel.models import Game, Player
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hi Henry')
+    if request.method == 'POST':
+        if not Game.objects.all().exists():
+            white, _ = Player.objects.get_or_create(username='white', password='', salt='')
+            black, _ = Player.objects.get_or_create(username='black', password='', salt='')
+            Game.objects.create(white=white, black=black, board='start')
+
+        game_id = Game.objects.all().last().id
+        return redirect('game', game_id=game_id)
+
+    return render(request, 'home.html', {})
+
+def game(request, game_id):
+    game = Game.objects.get(id=game_id)
+    submitted = False
+
+    if request.method == 'POST':
+        game.board = request.POST['fen']
+        game.save()
+        submitted = True
+
+    ctx = {
+        'fen': game.board,
+        'submitted': submitted,
+    }
+
+    return render(request, 'game.html', ctx)
